2706693a970bac771918b4b9786a3709c28cde4d
amj523 <amj523@york.ac.uk>
2015-01-28 11:36:56 +0000

    Another test commit. This time signed
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- amj523/src/HelloWorld.java
+++ amj523/src/HelloWorld.java
@@ -1,7 +1,22 @@
-
+/**
+ * Documentation comment
+ * @author amj523
+ *
+ */
 public class HelloWorld {
+	/* Class implementation comment */
+	
+	/**
+	 * Method documentation comment
+	 * @param args
+	 */
 	public static void main(String args[]) {
+		/* Method implementation comment */
+		
+		/* Implementation comment */
 		System.out.println("Hello Mother Fucking World");
+		
+		/* Implementation comment */
 		System.out.println("Wassup?");
 	}
 }
